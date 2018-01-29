1679576bd74ec1987e1edc09ea194489ae9718f0
Seth Barberee <seth.barberee@gmail.com>
2018-01-29 10:32:35 -0600

    got the heap portion to work

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -5,7 +5,7 @@ class Point {
         int x,y;
     public:
         void print(){
-            cout << "X: " << x << endl << "Y: " << y << endl;
+            cout << "X: " << x << " " << "Y: " << y << endl;
         }
         Point(int x, int y);
         Point();
@@ -18,22 +18,17 @@ Point :: Point(int x1, int y1) : x(x1), y(y1){}
 Point :: Point() : x(1), y(2){}
 
 int main(){
-    Point a = Point(1,1);
-    Point b = Point(2,1);
-    Point c = Point(3,4);
-    Point d = Point(4,5);
-    Point e = Point(6,7);
-    Point f = Point(4,8);
-    Point g = Point(3,5);
-    Point h = Point(10,3);
-    Point i = Point(1,2);
-    Point points[] = {a, b, c, d, e, f, g, h, i}; // declared on the stack
+    // Stack
+    Point points[] = {Point(1,1), Point(2,1), Point(3,4), Point(4,5), Point(6,7), Point(4,8), Point(3,5), Point(10,3), Point(1,2)}; // declared on the stack
     for (auto x : points){
         x.print();
     }
-    Point *ps = new Point[10]; // declared on the heap
-    // TODO: Iterate through array and then print
-    //for (Point* x : *ps){
-    //    x->print();
-    //}
+    // Heap
+    int num = 10; // size of the point array to make
+    Point *ps = new Point[num]; // declared on the heap
+    for (int i = 0; i < num; i++){
+        ps[i].print();
+    }
+    delete[] ps; // memory collection
+
 }
